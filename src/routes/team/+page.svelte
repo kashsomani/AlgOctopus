9b2747c8a2eaa294a5049d291276701eccb52b3e
Kash Somani <kash@kashsomani.com>
2023-01-28 13:39:39 +0530

    added basic card

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -1,4 +1,14 @@
 <script>
 </script>
 
-team
+<div class="card w-96 bg-primary shadow-xl">
+  <figure class="px-10 pt-10">
+    <img src="Origin.png" alt="Shoes" class="rounded-xl" />
+  </figure>
+  <div class="card-body items-center text-center">
+    <h2 class="card-title">Origin</h2>
+    <h3 class="card-title">Founder | Artist</h3>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions" />
+  </div>
+</div>
