49ca54fcff2a0b9a87d33232a794bdd67822ec52
calz18 <40797702+calz18@users.noreply.github.com>
2023-01-31 18:29:04 +0530

    team page

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -1,7 +1,10 @@
 <script>
+  import teams from "./_team/Teams.svelte";
 </script>
 
-<div class="card w-96 bg-primary shadow-xl">
+<Teams />
+
+<!-- <div class="card w-96 bg-primary shadow-xl">
   <figure class="px-10 pt-10">
     <img src="Origin.png" alt="Shoes" class="rounded-xl" />
   </figure>
@@ -11,4 +14,4 @@
     <p>If a dog chews shoes whose shoes does he choose?</p>
     <div class="card-actions" />
   </div>
-</div>
+</div> -->
---
+++ src/routes/team/_team/props.js
@@ -0,0 +1,27 @@
+export const props = [
+    {
+        image: "Origin.png",
+        title: "Origin",
+        points: [
+            "FOUNDER | ARTIST"
+        ],
+    },
+    {
+        image: "community_manager.png",
+        title: "StaxxOnDexx",
+        points: [
+            "RARE",
+            "SECOND STAGE OF INVASION",
+            "EARNS 200 $BLOP DAILY",
+        ],
+    },
+    {
+        image: "advisor.png",
+        title: "SlimDogg",
+        points: [
+            "LEGENDARY",
+            "FINAL STAGE OF INVASION",
+            "EARNS 300 $BLOP DAILY",
+        ],
+    },
+]
