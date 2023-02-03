1a9ec278785aaf1be2e1a19d06f42b2d5ac06acc
Kash Somani <kash@kashsomani.com>
2023-02-03 10:12:07 -0800

    more mobile

--- src/routes/team/_team/Team.svelte
+++ src/routes/team/_team/Team.svelte
@@ -1,23 +1,24 @@
 <script>
-  export let prop;
-  $: console.log(prop);
+  export let prop
+  $: console.log(prop)
 </script>
 
-<div class="card w-96 bg-primary shadow-xl" id="card">
+<div
+  class="card w-96 md:w-64 bg-primary shadow-xl"
+  id="card"
+>
   <figure class="px-7 pt-10">
     <img src={prop.image} alt="Shoes" class="rounded-xl" />
   </figure>
-  <div class="card-body items-center text-center">
+  <div class="card-body items-center text-center ">
     <h2 class="card-title">{prop.title}</h2>
-    <h3 class="card-title">{prop.points}</h3>
-    <!-- <p>If a dog chews shoes whose shoes does he choose?</p> -->
-    <div class="card-actions" />
+    <h3 class="card-title text-base">{prop.points}</h3>
   </div>
 </div>
 
 <style>
   #card {
-    border-bottom: 4px solid #5ce1e6;
-    box-shadow: 0 0 10px 8px #5ce1e6;
+    border-bottom: 4px solid var(--blue);
+    box-shadow: 0 0 10px 8px var(--blue);
   }
 </style>
--- src/routes/team/_team/Teams.svelte
+++ src/routes/team/_team/Teams.svelte
@@ -1,9 +1,11 @@
 <script>
-  import Team from "./Team.svelte";
-  import { props } from "./props";
+  import Team from "./Team.svelte"
+  import { props } from "./props"
 </script>
 
-<div class="grid grid-cols-1 place-items-center gap-6">
+<div
+  class="grid grid-cols-1 md:grid-cols-3 place-items-center gap-12"
+>
   {#each props as prop}
     <Team {prop} />
   {/each}
