55dbf4de9f4e445333f57a76aea696bf2a6c32fb
Kash Somani <kash@kashsomani.com>
2023-02-03 10:22:04 -0800

    border / shadow

--- src/routes/roadmap/+page.svelte
+++ src/routes/roadmap/+page.svelte
@@ -1,24 +1,27 @@
-<ol class="border-l-2 border-blue-600">
+<ol class="border-l-2 border-blue-600 glowy-boi">
   <li>
-    <div class="flex flex-start items-center">
+    <div class=" flex flex-start items-center">
       <div
-        class="bg-blue-600 w-4 h-4 flex items-center justify-center rounded-full -ml-2 mr-3 -mt-2"
+        class="round-boi bg-blue-600 w-4 h-4 flex items-center justify-center rounded-full -ml-2 mr-3 -mt-2"
       />
-      <h4 class="text-black-800 font-semibold text-xl -mt-2">
+      <h4
+        class="text-black-800 font-semibold text-xl -mt-2"
+      >
         Title of section 1
       </h4>
     </div>
-    <div class="ml-6 mb-6 pb-6">
+    <div class="ml-6 mb-6 pb-6 ">
       <a
         href="#!"
         class="text-blue-600 hover:text-blue-700 focus:text-blue-800 duration-300 transition ease-in-out text-sm"
         >4 February, 2022</a
       >
       <p class="text-white-700 mt-2 mb-4">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.
+        Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit, sed do eiusmod tempor incididunt ut labore et
+        dolore magna aliqua. Ut enim ad minim veniam, quis
+        nostrud exercitation ullamco laboris nisi ut aliquip
+        ex ea commodo consequat.
       </p>
       <button
         type="button"
@@ -32,7 +35,9 @@
       <div
         class="bg-blue-600 w-4 h-4 flex items-center justify-center rounded-full -ml-2 mr-3 -mt-2"
       />
-      <h4 class="text-black-800 font-semibold text-xl -mt-2">
+      <h4
+        class="text-black-800 font-semibold text-xl -mt-2"
+      >
         Title of section 2
       </h4>
     </div>
@@ -43,11 +48,13 @@
         >12 January, 2022</a
       >
       <p class="text-gray-700 mt-2 mb-4">
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-        explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
-        odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+        Sed ut perspiciatis unde omnis iste natus error sit
+        voluptatem accusantium doloremque laudantium, totam
+        rem aperiam, eaque ipsa quae ab illo inventore
+        veritatis et quasi architecto beatae vitae dicta
+        sunt explicabo. Nemo enim ipsam voluptatem quia
+        voluptas sit aspernatur aut odit aut fugit, sed quia
+        consequuntur magni dolores eos qui ratione
         voluptatem sequi nesciunt.
       </p>
       <button
@@ -73,11 +80,13 @@
         >27 December, 2021</a
       >
       <p class="text-gray-700 mt-2 mb-4">
-        At vero eos et accusamus et iusto odio dignissimos ducimus qui
-        blanditiis praesentium voluptatum deleniti atque corrupti quos dolores
-        et quas molestias excepturi sint occaecati cupiditate non provident,
-        similique sunt in culpa qui officia deserunt mollitia animi, id est
-        laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita
+        At vero eos et accusamus et iusto odio dignissimos
+        ducimus qui blanditiis praesentium voluptatum
+        deleniti atque corrupti quos dolores et quas
+        molestias excepturi sint occaecati cupiditate non
+        provident, similique sunt in culpa qui officia
+        deserunt mollitia animi, id est laborum et dolorum
+        fuga. Et harum quidem rerum facilis est et expedita
         distinctio.
       </p>
       <button
@@ -88,3 +97,15 @@
     </div>
   </li>
 </ol>
+
+<style lang="postcss">
+  .round-boi {
+    border: 4px solid var(--blue);
+    box-shadow: 1px 1px 5px 1px var(--blue);
+  }
+  .glowy-boi {
+    border-left: 4px solid var(--blue);
+    box-shadow: -15px 0px 17px -7px var(--blue);
+    @apply pt-3 rounded-xl;
+  }
+</style>
