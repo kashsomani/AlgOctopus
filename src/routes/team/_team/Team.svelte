4ea2d6bb921296ec1b7813ba126ee748a0e1bfb9
calz18 <40797702+calz18@users.noreply.github.com>
2023-02-03 14:11:11 +0530

    css

--- src/routes/team/_team/Team.svelte
+++ src/routes/team/_team/Team.svelte
@@ -3,7 +3,7 @@
   $: console.log(prop);
 </script>
 
-<div class="card w-96 bg-primary shadow-xl">
+<div class="card w-96 bg-primary shadow-xl" id="card">
   <figure class="px-7 pt-10">
     <img src={prop.image} alt="Shoes" class="rounded-xl" />
   </figure>
@@ -14,3 +14,10 @@
     <div class="card-actions" />
   </div>
 </div>
+
+<style>
+  #card {
+    border-bottom: 4px solid #5ce1e6;
+    box-shadow: 0 0 10px 8px #5ce1e6;
+  }
+</style>
