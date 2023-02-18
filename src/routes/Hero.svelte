7db1f6d652037516cc725ab8e3f7973095b556ce
Kash Somani <kash@kashsomani.com>
2023-02-17 18:35:00 -0800

    updated background

--- src/routes/Hero.svelte
+++ src/routes/Hero.svelte
@@ -2,15 +2,13 @@
   import Button from "$lib/Button.svelte"
 </script>
 
-<div class="hero h-96 bg-base-100">
+<div class="hero h-96">
   <div class="hero-content text-center">
     <div class="max-w-md">
-      <h1 class="text-5xl font-bold text-primary">
-        Join The Invasion!
-      </h1>
+      <h1 class="text-5xl font-bold text-primary">Join The Invasion!</h1>
       <p class="py-6">
-        Our V1 Collection contains 300 unique, hand drawn
-        Invading Algorand Mollusks!
+        Our V1 Collection contains 300 unique, hand drawn Invading Algorand
+        Mollusks!
       </p>
       <Button />
     </div>
